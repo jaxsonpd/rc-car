93988f91ae6570b8c1f38988f480854d04b9acc6
Ethan Wildash-Chan <ewi69@uclive.ac.nz>
2024-05-01 11:00:31 +1200

    Added roll-pwm titles to darstardly.c

--- src/apps/hat/dastardly.c
+++ src/apps/hat/dastardly.c
@@ -2,4 +2,20 @@
 // inputs: roll value (int16_t) <- !! changed from when we talked
 // outpus: pwm signal
 
+/** 
+ * @file dastardly.c
+ * @author Ethan Wildash-Chan (ewi69@uclive.ac.nz)
+ * @date 2024-05-01
+ * @brief Implements pwm repsonse to roll value input
+ */
 
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+void roll_to_pwm (int16_t roll) {
+    /* takes a roll value from the accelerometer and returns a
+    duty cycle for a pwm to be sent to the motor drivers*/ 
+    
+
+}
