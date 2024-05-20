f587f10253709947d053beff741a0a11be9621c0
Daniel Hawes <dha144@uclive.ac.nz>
2024-05-20 14:37:44 +1200

    Motors working with ramping

--- src/apps/racer/test_control.c
+++ src/apps/racer/test_control.c
@@ -19,14 +19,49 @@
 
 #include "target.h"
 
-#define DELAY_MS 10
+#define DELAY_MS 00
 #define NUM_LEDS 20
+
+#define RAMP_STEP 1 
+#define RAMP_DELAY 10
 
 #define PACER_RATE 50
 // #define RX_RATE 15
 // #define TX_RATE 8
 
 bool button_press = false;
+
+void ramp_duty_cycle(int *current_left_duty, int target_left_duty, int *current_right_duty, int target_right_duty) {
+    while (*current_left_duty != target_left_duty || *current_right_duty != target_right_duty) {
+        // printf("Left Motor: %d, Right Motor: %d", current_left_duty, current_right_duty);
+        if (*current_left_duty < target_left_duty) {
+            *current_left_duty += RAMP_STEP;
+            if (*current_left_duty > target_left_duty) {
+                *current_left_duty = target_left_duty;
+            }
+        } else if (*current_left_duty > target_left_duty) {
+            *current_left_duty -= RAMP_STEP;
+            if (*current_left_duty < target_left_duty) {
+                *current_left_duty = target_left_duty;
+            }
+        }
+
+        if (*current_right_duty < target_right_duty) {
+            *current_right_duty += RAMP_STEP;
+            if (*current_right_duty > target_right_duty) {
+                *current_right_duty = target_right_duty;
+            }
+        } else if (*current_right_duty > target_right_duty) {
+            *current_right_duty -= RAMP_STEP;
+            if (*current_right_duty < target_right_duty) {
+                *current_right_duty = target_right_duty;
+            }
+        }
+
+        set_duty(*current_left_duty, *current_right_duty); // Update both motors
+        delay_ms(RAMP_DELAY);
+    }
+}
 
 /***
  * adapts h bridge main to include init radio and ready to recieve radio
@@ -72,8 +107,8 @@ int main (void)
     pio_config_set(BUMP_DETECT, PIO_PULLUP);
 
     // creates variables for setting duty
-    int left_duty;
-    int right_duty;
+    int prev_left_duty = 0;
+    int prev_right_duty = 0;
     int dastardly;
     int parity;
     uint8_t hit_signal;
@@ -92,67 +127,29 @@ int main (void)
             int left_motor_duty;
             int right_motor_duty;
             if (sscanf(buf, "%d %d", &left_motor_duty, &right_motor_duty) == 2){
-                if (left_motor_duty >= 10) {
+                // set_duty(0,0);
+                printf("Left Motor: %d Right Motor %d\n", left_motor_duty, right_motor_duty);
+                if (left_motor_duty >= 80) {
                     left_motor_duty = 10;
-                } else if (left_motor_duty <= -10) {
+                } else if (left_motor_duty <= -80) {
                     left_motor_duty = -10;
                 }
-                if (right_motor_duty >= 10) {
+                if (right_motor_duty >= 80) {
                     right_motor_duty = 10;
-                } else if (right_motor_duty <= -10) {
+                } else if (right_motor_duty <= -80) {
                     right_motor_duty = -10;
                 }
-                set_duty(left_motor_duty, right_motor_duty);
+                if (right_motor_duty == 0 || left_motor_duty == 0) {
+                    set_duty(0, 0);
+                } else {
+                    ramp_duty_cycle(&prev_left_duty, left_motor_duty, &prev_right_duty, right_motor_duty);
+                    prev_right_duty=right_motor_duty;
+                    prev_left_duty=left_motor_duty;
+                }
+                // set_duty(0, 0);
             }
         }
 
-        ledtape_write (LEDTAPE_PIO, leds, NUM_LEDS*3);
-        // tick_rx++;
-        // tick_tx++;
-        
-
-        // if(tick_tx > (PACER_RATE/TX_RATE)) {
-        //     pio_output_set(TX_LED, 0); // tells its in tranmitting mode
-        //     pio_output_set (RX_LED, 1);
-        //     int tx_status;
-
-        //     //check for button press
-        //     bool hit_detect = pio_input_get (BUMP_DETECT);
-
-        //     tx_status = radio_tx(hit_detect);
-        //     pio_output_set(TX_LED, 1);
-
-        //     // if (tx_status == 1){
-        //     //     // listening == true;   // once message has been sent, board is back in receiving mode
-        //     //     pio_output_set(TX_LED, 1);
-        //     // }
-            
-        //     tick_tx = 0;
-        // } 
-
-        // if (tick_rx > (PACER_RATE/RX_RATE)) {
-        //     pio_output_set (RX_LED, 0);
-        //     pio_output_set(TX_LED, 1);
-
-        //     char buffer[32] = {0};
-
-        //     radio_rx(buffer);
-
-        //     printf("%s\n", buffer);
-        //     printf("TX\n");
-
-        //     sscanf(buffer, "%d,%d,%d,%d", &left_duty, 
-        //     &right_duty,&dastardly,&parity);
-
-        //     printf("Set: %3d,%3d,%2d,%1d\n", left_duty, right_duty, dastardly, 
-        //     parity);
-
-        //     set_duty(left_duty, right_duty);
-            
-        //     tick_rx = 0;
-        //     // printf ("listening\n");
-        }
-
-        
+        ledtape_write (LEDTAPE_PIO, leds, NUM_LEDS*3);        
     }
 }
